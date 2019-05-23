984dde189ba5780ae0c1a9385d09ce866acb9e42
Timothee Simon <xorob0@posteo.net>
2019-05-23 18:06:26 +0200

    Added Helmet imports

--- src/pages/coffees.js
+++ src/pages/coffees.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import { Welcome } from "../components/dumb/welcome"
 import { Board } from "../components/dumb/board"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import { Welcome } from "../components/dumb/welcome"
 import { Quote } from "../components/dumb/quote"
