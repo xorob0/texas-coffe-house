9c4898c115818bece51006d4046a8ef195e22dcc
Timothee Simon <xorob0@posteo.net>
2019-05-23 17:38:39 +0200

    Food page

--- src/pages/food.js
+++ src/pages/food.js
@@ -1,8 +1,27 @@
 import React from "react"
 
 import { Welcome } from "../components/dumb/welcome"
-import { Quote } from "../components/dumb/quote"
-import { Preview } from "../components/dumb/preview"
+import { Board } from "../components/dumb/board"
+
+const ITEMS = [
+  { name: "John ayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  {
+    name: "John Wayne",
+    description: "When Hot meet ColdWhen Hot meet ColdWhen Hot meet Cold",
+    price: "3,5",
+  },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+  { name: "John Wayne", description: "When Hot meet Cold", price: "3,5" },
+]
 
 const IndexPage = () => (
   <>
@@ -10,6 +29,9 @@ const IndexPage = () => (
       title="Our Coffees"
       description="Try the John Wayne! It’s an exeperience !"
     />
+    <Board items={ITEMS} title="Our pastery" first={true} />
+    <Board items={ITEMS} title="Our superbowl" />
+    <Board items={ITEMS} title="Our waffles" />
   </>
 )
 
