dfad10933b0bc418e4b5bf7f91723c0b354a9133
Timothee Simon <xorob0@posteo.net>
2019-06-05 16:15:01 +0200

    Added service worker with notification

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,3 +13,33 @@ export const onClientEntry = () => {
 export const wrapRootElement = ({ element }) => (
   <UserProvider>{element}</UserProvider>
 )
+
+export const onServiceWorkerUpdateFound = () => {
+  const showNotification = () => {
+    Notification.requestPermission(result => {
+      if (result === "granted") {
+        navigator.serviceWorker.ready.then(registration => {
+          registration.showNotification("Update", {
+            body: "New content is available!",
+            icon: "link-to-your-icon",
+            vibrate: [200, 100, 200, 100, 200, 100, 400],
+            tag: "request",
+            actions: [
+              // you can customize these actions as you like
+              {
+                action: console.log("test"), // you should define this
+                title: "update",
+              },
+              {
+                action: console.log("ttt"), // you should define this
+                title: "ignore",
+              },
+            ],
+          })
+        })
+      }
+    })
+  }
+
+  showNotification()
+}
