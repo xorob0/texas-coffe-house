2bf07343b950d5cdf8e5477d022260fc58fa5c1f
Timothee Simon <xorob0@posteo.net>
2019-06-05 18:05:26 +0200

    Trying with other import

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js")
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js")
+importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js")
 
 // Required stuff
 importScripts(
